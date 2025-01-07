8c799f83654c1d5c2bec97bb6a69c1ea841eecd3
SteeperMold <112201965+SteeperMold@users.noreply.github.com>
2025-01-07 19:03:56 +0400

    update deploy.yaml

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-  baseURL: process.env.SERVER_ADDRESS || "http://localhost:8080",
+  baseURL: process.env.REACT_APP_SERVER_IP || "http://localhost:8080",
   headers: {
     "Content-Type": "application/json",
   },
